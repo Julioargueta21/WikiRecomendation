3fd622ce47506f3045b82160244f26a3eb3135df
Miguel Cruz <Miguelc4600@gmail.com>
2019-03-07 14:37:29 -0500

    Latest code

--- src/BackEnd/HTMLParser.java
+++ src/BackEnd/HTMLParser.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 public class HTMLParser {
 
     public static String grabWebPage() throws IOException {
-        Document webDoc = Jsoup.connect("https://en.wikipedia.org/wiki/Steve_Jobs").userAgent("Mozilla").data("name", "jsoup").get();
+        Document webDoc = Jsoup.connect("https://en.wikipedia.org/wiki/Mario").userAgent("Mozilla").data("name", "jsoup").get();
         Elements webElements = webDoc.select("div#mw-content-text");
         //This Makes the out.file (But we need to read from a control file)
         //PrintStream fileOut = new PrintStream(new File("control.txt"));
@@ -23,6 +23,7 @@ public class HTMLParser {
         }
         return elements;
     }
+
     // Counts Amount of words in array and puts words with keys on a hashmap
     public static HashMap countStrings(String[] wordList) {
         HashMap map = new HashMap();
@@ -42,34 +43,43 @@ public class HTMLParser {
         }
         return map;
     }
+
     public static TreeMap sortDescending(HashMap unSortedWordMap) {
         DescendingSort comp = new DescendingSort(unSortedWordMap);
         TreeMap sortedMap = new TreeMap<>(comp);
         sortedMap.putAll(unSortedWordMap);
         return sortedMap;
     }
+
     public static void printArrayVertically(String[] test) {
         for (String s : test) {
             System.out.println(s);
         }
     }
+
     public static void printTreeMapVertically(TreeMap tree) {
         for (Object key : tree.keySet()) {
             System.out.println(key + " = " + tree.get(key));
         }
     }
+
     public static String[] filterStrings(String string, String regex) {
         String[] outPutArray = string.split(regex);
         return outPutArray;
     }
+
     public static void rankEntries(TreeMap tree, int rank) {
     }
+
     public static void run(String regex) throws IOException {
         printTreeMapVertically(sortDescending(countStrings(filterStrings(grabWebPage(), regex))));
     }
-    public static void main(String[] args) throws IOException {
-        run("[^a-zA-Z]+");
-    }
 
+    public static void main(String[] args) throws IOException {
+        //System.out.print(grabWebPage());
+        //run("[^a-zA-Z]+"+"\\b"+"[\\w]{0,4}"+"\\b");
+        run("[^a-zA-Z\\s]+|\\b\\w{0,4}\\b");
+
+    }
 }
 
