1e87ff9c4ad215c37417e1a5b1d1cadf5c5a1407
Miguel Cruz <Miguelc4600@gmail.com>
2019-03-06 21:31:30 -0500

    Act 1

---
+++ src/BackEnd/HTMLParseSort.java
@@ -0,0 +1,72 @@
+package BackEnd;
+
+import com.sun.source.tree.Tree;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+
+public class HTMLParseSort {
+
+    public static String grabWebPage() throws IOException {
+        Document webDoc = Jsoup.connect("https://en.wikipedia.org/wiki/Steve_Jobs").userAgent("Mozilla").data("name", "jsoup").get();
+        Elements webElements = webDoc.select("div#mw-content-text");
+
+        //This Makes the out.file (But we need to read from a control file)
+
+        //PrintStream fileOut = new PrintStream(new File("control.txt"));
+        //System.setOut(fileOut);
+        String elements = null;
+        for (Element el : webElements) {
+            elements = el.text(); }
+        return elements;
+    }
+
+    static String[] test = {"test", "test", "test",  "SingularWord"};
+
+    public void filterStrings(){
+
+    }
+
+    public static HashMap countStrings(String[] wordList){
+        HashMap map = new HashMap();
+        Integer ONE = new Integer(1);
+
+        for (int i=0, n=wordList.length; i<n; i++) {
+            String key = wordList[i];
+            Integer frequency = (Integer)map.get(key);
+            if (frequency == null) {
+                frequency = ONE;
+            } else {
+                int value = frequency.intValue();
+                frequency = new Integer(value + 1);
+            }
+            map.put(key, frequency);
+
+    }
+        return map;
+    }
+
+
+    public static TreeMap parseSort(HashMap unSortedWordMap){
+        TreeMap sortedMap = new TreeMap(unSortedWordMap);
+        return sortedMap;
+    }
+
+
+        public static void main(String[] args) throws IOException  {
+        System.out.println(grabWebPage());
+            System.out.println(parseSort(countStrings(test)));
+
+        }
+
+
+
+    }
+
---
+++ src/GUI/UI.java
@@ -0,0 +1,9 @@
+package GUI;
+import javax.swing.*;
+
+public class UI {
+
+    public static void main(String[] args) {
+
+    }
+}
